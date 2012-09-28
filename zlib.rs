a4bdb2a54d49995d18a95ee5adc539cf32f54cf4
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2012-09-28 08:16:55 -0700

    Update to rust HEAD, add error handling

--- zlib.rs
+++ zlib.rs
@@ -1,50 +1,100 @@
-use std;
+extern mod std;
 
-import core::ptr;
-import core::vec;
-
-import libc::c_int;
-
-export compress, uncompress;
+use libc::{c_int, c_ulong};
 
 #[link_name = "z"]
-#[abi = "cdecl"]
 extern mod libz {
-    fn compressBound(len: u64) -> u64;
-    fn compress2(dest: *u8, destlen: *u64, src: *u8, srclen: u64,
+    fn compressBound(len: c_ulong) -> c_ulong;
+    fn compress2(dest: *mut u8, destlen: &mut c_ulong,
+                 src: *u8, srclen: c_ulong,
                  level: c_int) -> c_int;
-    fn uncompress(dest: *u8, destlen: *u64, src: *u8, srclen: u64) -> c_int;
+    fn uncompress(dest: *mut u8, destlen: &mut c_ulong,
+                  src: *u8, srclen: c_ulong) -> c_int;
 }
 
-fn compress(src: ~[u8], level: int) -> ~[u8] unsafe {
-    assert(level >= 0);
-    assert(level <= 9);
-    let srclen = vec::len(src) as u64;
-    let destlen = libz::compressBound(srclen);
-    let dest = vec::to_mut(vec::from_elem::<u8>(destlen as uint, 0u8));
-    let pdest = vec::unsafe::to_ptr::<u8>(dest);
-    let psrc = vec::unsafe::to_ptr::<u8>(src);
-    let pdestlen = ptr::addr_of::<u64>(destlen);
-    let r = libz::compress2(pdest, pdestlen, psrc, srclen, level as c_int);
-    // XXX: 0 == Z_OK
-    if r != 0 as c_int {
-        fail #fmt["zlib compress2() returned %d", r as int];
-    }
-    vec::slice::<u8>(dest, 0u, destlen as uint)
+pub enum Error {
+    StreamError,
+    DataError,
+    MemError,
+    BufError,
+    UnknownError(int),
 }
 
-fn uncompress(src: ~[u8], _destlen: u64) -> ~[u8] unsafe {
-    let srclen = vec::len(src) as u64;
-    let destlen = _destlen;
-    let pdestlen = ptr::addr_of::<u64>(destlen);
-    let dest = vec::to_mut(vec::from_elem::<u8>(destlen as uint, 0u8));
-    let pdest = vec::unsafe::to_ptr::<u8>(dest);
-    let psrc = vec::unsafe::to_ptr::<u8>(src);
-    let r = libz::uncompress(pdest, pdestlen, psrc, srclen);
-    if r != 0 as c_int {
-        fail #fmt["zlib uncompress() returned %d", r as int];
+pub fn compress(src: &[u8], level: Option<int>) -> Result<~[u8], Error> {
+    let level = match level {
+        None => -1,
+        Some(level) => level,
+    };
+
+    let bounds = libz::compressBound(src.len() as c_ulong) as uint;
+    let mut dest = vec::with_capacity(bounds);
+
+    let r = do vec::as_imm_buf(src) |psrc, srclen| {
+        do vec::as_mut_buf(dest) |pdest, _destlen| {
+            let mut destlen = bounds as c_ulong;
+
+            let r = libz::compress2(
+                pdest, &mut destlen,
+                psrc, srclen as c_ulong,
+                level as c_int
+            ) as int;
+
+            // Where 0 == Z_OK
+            if r == 0 {
+                Ok(destlen as uint)
+            } else {
+                Err(convert_error(r))
+            }
+        }
+    };
+
+    match r {
+        Ok(destlen) => {
+            unsafe { vec::raw::set_len(dest, destlen); }
+            Ok(dest)
+        }
+        Err(e) => Err(e)
     }
-    vec::slice::<u8>(dest, 0u, destlen as uint)
+}
+
+pub fn uncompress(src: &[u8], destlen: uint) -> Result<~[u8], Error> {
+    let mut dest = vec::with_capacity(destlen);
+
+    let r = do vec::as_imm_buf(src) |psrc, srclen| {
+        do vec::as_mut_buf(dest) |pdest, _destlen| {
+            let mut destlen = destlen as c_ulong;
+
+            let r = libz::uncompress(
+                pdest, &mut destlen,
+                psrc, srclen as c_ulong
+            ) as int;
+
+            // Where 0 == Z_OK
+            if r == 0 {
+                Ok(destlen as uint)
+            } else {
+                Err(convert_error(r))
+            }
+        }
+    };
+
+    match r {
+        Ok(destlen) => {
+            unsafe { vec::raw::set_len(dest, destlen); }
+            Ok(dest)
+        }
+        Err(e) => Err(e)
+    }
+}
+
+fn convert_error(r: int) -> Error {
+    match r {
+        -2 => StreamError,
+        -3 => DataError,
+        -4 => MemError,
+        -5 => BufError,
+        _ => UnknownError(r as int)
+    }
 }
 
 #[cfg(test)]
@@ -52,8 +102,8 @@ mod tests {
     #[test]
     fn test() {
         let d = ~[0xdeu8, 0xadu8, 0xd0u8, 0x0du8];
-        let c = compress(d, 9);
-        let r = uncompress(c, vec::len(d) as u64);
-        assert(r == d);
+        let c = result::unwrap(compress(d, Some(9)));
+        let r = result::unwrap(uncompress(c, d.len()));
+        assert r == d;
     }
 }
